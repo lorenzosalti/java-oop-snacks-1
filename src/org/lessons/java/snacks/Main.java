3de9a938ff3b90b19c54e824a7aac66e45330910
lorenzosalti <lorenzosalti@gmail.com>
2025-06-29 18:59:28 +0200

    add: 4 Studente constructed and infos printed in console

--- src/org/lessons/java/snacks/Main.java
+++ src/org/lessons/java/snacks/Main.java
@@ -2,6 +2,14 @@ package org.lessons.java.snacks;
 
 public class Main {
   public static void main(String[] args) {
+    Studente mario = new Studente("Paolo", "Idraulici", 23);
+    Studente luigi = new Studente("Luigi", "Idraulici", 21);
+    Studente pesca = new Studente("Pesca", "Reali", 22);
+    Studente margherita = new Studente("Margherita", "Reali", 20);
 
+    System.out.println(mario.getStudenteInfos());
+    System.out.println(luigi.getStudenteInfos());
+    System.out.println(pesca.getStudenteInfos());
+    System.out.println(margherita.getStudenteInfos());
   }
 }
