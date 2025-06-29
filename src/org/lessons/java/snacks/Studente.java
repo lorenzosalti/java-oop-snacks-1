dca2fcf3979317643f3452c2bf6f3813d5d78d6c
lorenzosalti <lorenzosalti@gmail.com>
2025-06-29 18:46:51 +0200

    add: Studente attributes, constructor and basic methods

--- src/org/lessons/java/snacks/Studente.java
+++ src/org/lessons/java/snacks/Studente.java
@@ -2,4 +2,47 @@ package org.lessons.java.snacks;
 
 public class Studente {
 
+  // ATTRIBUTES
+  private String firstName;
+  private String lastName;
+  private int age;
+
+  // CONSTRUCTORS
+  protected Studente(String firstName, String lastName, int age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  // METHODS
+  public String getFirstName() {
+    return this.firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    if (firstName != null) {
+      this.firstName = firstName;
+    }
+  }
+
+  public String getLastName() {
+    return this.lastName;
+  }
+
+  public void setLastName(String lastName) {
+    if (lastName != null) {
+      this.lastName = lastName;
+    }
+  }
+
+  public int getAge() {
+    return this.age;
+  }
+
+  public void setAge(int age) {
+    if (age != 0) {
+      this.age = age;
+    }
+  }
+
 }
